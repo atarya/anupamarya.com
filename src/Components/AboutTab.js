53f9e97c19a2a924198ad81b3cc7a841745e2edb
aarya <anupam@apostrfy.com>
2023-10-03 00:05:11 +0530

    Website v1.0

--- src/Components/AboutTab.js
+++ src/Components/AboutTab.js
@@ -4,38 +4,38 @@ import "../Styles/AboutTab.css"
 function AboutTab() {
   return (
     <div className="about-container">
-      <img src="path_to_default_image.jpg" alt="Profile" className="profile-photo" />
-      <h2>Anupam Arya</h2>
-      <p>Dummy text line 1.</p>
+      <div className="center-container">
+        <img src={process.env.PUBLIC_URL + "/profile.jpg"} alt="Profile" className="profile-photo" />
+      </div>
+      <div class="name-container">
+        <h1>Anupam Arya</h1>
+        <img src={process.env.PUBLIC_URL + "/verified.png"} alt="Verified Badge" class="verified-badge" />
+      </div>
+      <p className="intro">Hey there! I'm Anupam. Dive into my world of content quirks and coding chronicles. Got a fun project? Reach out to collaborate on content or code. Curious about my recent escapades? Just hit 'Now'. Beware, they might just be the talk of the town soon!</p>
       <p className="email">business@anupamarya.com</p>
       <div className="social-icons">
-        <div className="single-icon">
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-            <img src="path_to_linkedin_icon.jpg" alt="LinkedIn" />
-            <span>LinkedIn</span>
+        <div className="social-main">
+          <a href="https://www.youtube.com/@anupamarya" target="_blank" rel="noopener noreferrer">
+          <img src={process.env.PUBLIC_URL + "/youtube.png"} alt="YouTube" />
           </a>
         </div>
-        <div className="multi-icons">
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-            <img src="path_to_linkedin_icon.jpg" alt="LinkedIn" />
-            <span>LinkedIn</span>
+        <div className="social-row">
+          <a href="https://www.instagram.com/_anupamarya" target="_blank" rel="noopener noreferrer">
+            <img src={process.env.PUBLIC_URL + "/instagram.png"} alt="Instagram" />
           </a>
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-            <img src="path_to_linkedin_icon.jpg" alt="LinkedIn" />
-            <span>LinkedIn</span>
+          <a href="https://twitter.com/_anupamarya" target="_blank" rel="noopener noreferrer">
+            <img src={process.env.PUBLIC_URL + "/twitter.png"} alt="X" />
           </a>
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-            <img src="path_to_linkedin_icon.jpg" alt="LinkedIn" />
-            <span>LinkedIn</span>
+          <a href="https://www.linkedin.com/in/reachanupam" target="_blank" rel="noopener noreferrer">
+            <img src={process.env.PUBLIC_URL + "/linkedin.png"} alt="LinkedIn" />
           </a>
-          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
-            <img src="path_to_linkedin_icon.jpg" alt="LinkedIn" />
-            <span>LinkedIn</span>
+          <a href="https://discord.com/channels/@anupamarya" target="_blank" rel="noopener noreferrer">
+            <img src={process.env.PUBLIC_URL + "/discord.png"} alt="Discord" />
           </a>
         </div>
       </div>
       <footer>
-        Copyright 2023
+        <p>Copyright &copy; 2023</p>
       </footer>
     </div>
   );
--- src/Components/NowTab.js
+++ src/Components/NowTab.js
@@ -1,12 +1,55 @@
 import React from 'react';
+import "../Styles/NowTab.css"
 
 function NowTab() {
+  const activities = [
+    {
+      description: "Launched this really cool website.",
+      date: "Oct 2, 2023",
+      image: process.env.PUBLIC_URL + "/website.png",
+      link: "https://www.anupamarya.com"
+    },
+    {
+      description: "Bought a ₹250000000 lottery ticket.",
+      date: "Sep 10, 2023",
+      image: process.env.PUBLIC_URL + "/lottery.png",
+      link: "https://www.youtube.com/watch?v=KYQ6Gu-zhrg"
+    },
+    {
+      description: "Watched Oppenheimer in the biggest cinema screen in India.",
+      date: "Jul 27, 2023",
+      image: process.env.PUBLIC_URL + "/oppenheimer.png",
+      link: "https://www.youtube.com/watch?v=u2nPkwlUI6Y"
+    },
+  ];
+
   return (
-    <div>
-      <h2>Now Content</h2>
-      <p>This is the content for the Now tab.</p>
+    <div className="now-container">
+      <h1>{"Events [Live]"}</h1>
+      <div className="now-table-container">
+      <table className="activity-table">
+        <tbody>
+          {activities.map((activity, index) => (
+            <tr key={index} className="activity-row">
+              <td className="activity-desc">
+                <a href={activity.link} target="_blank" rel="noopener noreferrer">
+                  {activity.description}
+                </a>
+              </td>
+              <td className="activity-date">{activity.date}</td>
+              <td className="activity-img-cell">
+                <a href={activity.link} target="_blank" rel="noopener noreferrer">
+                  <img src={activity.image} alt={activity.description} className="activity-img" />
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      </div>
     </div>
   );
 }
+
 
 export default NowTab;
